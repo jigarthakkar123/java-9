830f980675c9175f181470768c5885eb4ee1da99
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2024-11-20 09:57:19 +0530

    20-11-2024

--- IfCondition.java
+++ IfCondition.java
@@ -60,6 +60,40 @@
 
 package com.core;
 
+import java.util.Scanner;
+
 public class IfCondition {
 
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter A : ");
+		a=sc.nextInt();
+		
+		System.out.print("Enter B : ");
+		b=sc.nextInt();
+		
+		System.out.print("Enter C : ");
+		c=sc.nextInt();
+		
+		if(a>b)
+		{
+			if(a>c)
+			{
+				System.out.println("A Is Max Number");
+			}
+			else
+			{
+				System.out.println("C Is Max Number");
+			}
+		}
+		else if(b>c)
+		{
+			System.out.println("B is Max Number");
+		}
+		else
+		{
+			System.out.println("C Is Max Number");
+		}
+	}
 }
